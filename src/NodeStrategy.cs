8848c115124884e5581f79cdaa7ca8ce1959341d
Mike Minutillo <michael.minutillo@gmail.com>
2015-11-27 11:09:39 +0800

    Split Message and Endpoint Node strategies. Default behavior is Logical for Endpoint Nodes and Collapse to Sender for Message Nodes

--- src/NodeStrategy.cs
+++ src/NodeStrategy.cs
@@ -1,36 +1,48 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace RoutingVisualization
 {
-    public abstract class NodeStrategy
+    public class NodeStrategy : INodeStrategy<EndpointDetails>, INodeStrategy<ProcessedMessage>
     {
-        public abstract string GetNodeId(EndpointDetails details);
+        private INodeStrategy<EndpointDetails> _endpointNodeStrategy;
+        private INodeStrategy<ProcessedMessage> _messageNodeStrategy;
 
-        public string GetNodeId(ProcessedMessage message)
+        public NodeStrategy(INodeStrategy<EndpointDetails> endpointNodeStrategy, INodeStrategy<ProcessedMessage> messageNodeStrategy)
         {
-            var intent = message.Headers["NServiceBus.MessageIntent"];
-
-            if (intent == "Publish")
-            {
-                return ToNodeName(GetNodeId(message.MessageMetadata.SendingEndpoint), intent,
-                    message.MessageMetadata.MessageType);
-            }
-            else
-            {
-                return ToNodeName(GetNodeId(message.MessageMetadata.ReceivingEndpoint), intent,
-                    message.MessageMetadata.MessageType);
-            }
-
-            //return ToNodeName(GetNodeId(message.MessageMetadata.SendingEndpoint), intent, message.MessageMetadata.MessageType);
+            _endpointNodeStrategy = endpointNodeStrategy;
+            _messageNodeStrategy = messageNodeStrategy;
         }
+
+        public string GetNodeId(EndpointDetails details)
+        {
+            return _endpointNodeStrategy.GetNodeId(details);
+        }
+
+        public string GetNodeId(ProcessedMessage details)
+        {
+            return _messageNodeStrategy.GetNodeId(details);
+        }
+    }
+
+    public interface INodeStrategy<in T> 
+    {
+        string GetNodeId(T details);
+    }
+
+    public abstract class NodeStrategy<T> : INodeStrategy<T>
+    {
+        public abstract string GetNodeId(T details);
 
         protected static string ToNodeName(params string[] parts)
         {
             return Regex.Replace(string.Join("_", parts), "[-.]", "_").ToLower();
         }
     }
+    
 
-    class PhysicalRoutingNodeStrategy : NodeStrategy
+    class PhysicalRoutingNodeStrategy : NodeStrategy<EndpointDetails>
     {
         public override string GetNodeId(EndpointDetails details)
         {
@@ -38,7 +50,7 @@ namespace RoutingVisualization
         }
     }
 
-    class LogicalRoutingNodeStrategy : NodeStrategy
+    class LogicalRoutingNodeStrategy : NodeStrategy<EndpointDetails>
     {
         public override string GetNodeId(EndpointDetails details)
         {
@@ -46,4 +58,64 @@ namespace RoutingVisualization
         }
     }
 
+    class CollaseMessagesFromSameSenderMessageNodeStrategy : NodeStrategy<ProcessedMessage>
+    {
+        private NodeStrategy<EndpointDetails> _endpointNodeStrategy;
+
+        public CollaseMessagesFromSameSenderMessageNodeStrategy(NodeStrategy<EndpointDetails> endpointNodeStrategy)
+        {
+            _endpointNodeStrategy = endpointNodeStrategy;
+        }
+
+        public override string GetNodeId(ProcessedMessage details)
+        {
+            var intent = details.Headers["NServiceBus.MessageIntent"];
+            var sendingEndpointNodeId = _endpointNodeStrategy.GetNodeId(details.MessageMetadata.SendingEndpoint);
+            return ToNodeName(sendingEndpointNodeId, intent, details.MessageMetadata.MessageType);
+        }
+    }
+
+    class CollapseMessagesToSameReceiverMessageNodeStrategy : NodeStrategy<ProcessedMessage>
+    {
+        private NodeStrategy<EndpointDetails> _endpointNodeStrategy;
+
+        public CollapseMessagesToSameReceiverMessageNodeStrategy(NodeStrategy<EndpointDetails> endpointNodeStrategy)
+        {
+            _endpointNodeStrategy = endpointNodeStrategy;
+        }
+
+        public override string GetNodeId(ProcessedMessage details)
+        {
+            var intent = details.Headers["NServiceBus.MessageIntent"];
+            var receivingEndpointNodeId = _endpointNodeStrategy.GetNodeId(details.MessageMetadata.ReceivingEndpoint);
+            return ToNodeName(receivingEndpointNodeId, intent, details.MessageMetadata.MessageType);
+        }
+    }
+
+    class IntentBasedMessageNodeStrategy : INodeStrategy<ProcessedMessage>
+    {
+        private IDictionary<string, INodeStrategy<ProcessedMessage>> _strategyMap = new Dictionary<string, INodeStrategy<ProcessedMessage>>(StringComparer.InvariantCultureIgnoreCase);
+        private INodeStrategy<ProcessedMessage> _defaultStrategy;
+
+        public IntentBasedMessageNodeStrategy(INodeStrategy<ProcessedMessage> defaultStrategy)
+        {
+            _defaultStrategy = defaultStrategy;
+        }
+
+        public string GetNodeId(ProcessedMessage details)
+        {
+            var intent = details.Headers["NServiceBus.MessageIntent"];
+
+            INodeStrategy<ProcessedMessage> strategy;
+            if (!_strategyMap.TryGetValue(intent, out strategy))
+                strategy = _defaultStrategy;
+
+            return strategy.GetNodeId(details);
+        }
+
+        public void Add(string intent, INodeStrategy<ProcessedMessage> strategy)
+        {
+            _strategyMap.Add(intent, strategy);
+        }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -23,11 +23,7 @@ namespace RoutingVisualization
             store.Initialize();
             Console.WriteLine($"Reading messages from {store.Url}");
 
-            var modelBuilder = new ModelBuilder(
-                // Physical Routing will split up endpoints that have the same name but are on different hosts
-                // new PhysicalRoutingNodeStrategy()
-                new LogicalRoutingNodeStrategy()
-            );
+            var modelBuilder = new ModelBuilder(GetNodeStrategy());
 
             var dataSource = new AllProcessedMessagesRoutedMessageSource(store);
 
@@ -40,6 +36,31 @@ namespace RoutingVisualization
             dgml.Save(outputFileName);
 
             Console.WriteLine($"Created {outputFileName}");
+        }
+
+        private static NodeStrategy GetNodeStrategy()
+        {
+            // Logical Routing Strategy collapses endpoints with the same name into a single endpoint. Even if running on different hosts
+            var logicalEndpoints = new LogicalRoutingNodeStrategy();
+            // Physical Routing Strategy will split up endpoints that have the same name but run on different hosts
+            //var physicalEndpoints = new PhysicalRoutingNodeStrategy();
+
+            var endpoints = logicalEndpoints;
+            
+            // All messages of the same type that come from the same sender will be collapsed into a single message node
+            var collapseFromSender = new CollaseMessagesFromSameSenderMessageNodeStrategy(endpoints);
+
+            // All messages of the same type that go to the same receiver will be collapsed into a single message node
+            //var collapseToReceiver = new CollapseMessagesToSameReceiverMessageNodeStrategy(endpoints);
+
+            // All published events of the same type and sender will collapse into one, 
+            // all non-events of the same type and sent to the same reciever collapses into one
+            //var intentBasedMessageNodeStrategy = new IntentBasedMessageNodeStrategy(collapseToReceiver);
+            //intentBasedMessageNodeStrategy.Add("Publish", collapseFromSender);
+
+            var messages = collapseFromSender;
+
+            return new NodeStrategy(endpoints, messages);
         }
     }
 }
